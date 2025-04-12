9f9bf0c1d72be74b592750d79bf81e4222ba72db
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2025-04-12 17:22:15 -0300

    feat: create precompute adjance mines

--- game/tests/unit/test_mines.py
+++ game/tests/unit/test_mines.py
@@ -1,4 +1,4 @@
-from game.src.mechanics.mines import randomly_generate_mines
+from game.src.mechanics.mines import randomly_generate_mines, precompute_adjacent_mines
 
 def should_randomly_generate_mines_positions_given_rows_cols_and_a_number_of_expected_mines():
     expected_positions = {(3, 8), (4, 3), (0, 0), (1, 8), (1, 0), (1, 6), (3, 2), (6, 3), (1, 3), (0, 8)}
@@ -6,3 +6,16 @@ def should_randomly_generate_mines_positions_given_rows_cols_and_a_number_of_exp
     seed = 42
     mines_positions = randomly_generate_mines(rows, cols, mines_count=10, seed=seed)
     assert mines_positions == expected_positions
+
+def should_precompute_adjacent_mines_and_return_real_board_given_a_visible_board():
+    visible_board = [
+        ['.', 'M', '.'],
+        ['.', '.', '.'],
+        ['.', '.', 'M']
+    ]
+    mines_positions = set([(0, 1), (2, 2)])
+    real_board = precompute_adjacent_mines(visible_board, mines_positions)
+
+    expected_real_board = [['1', 'M', '1'], ['1', '2', '2'], ['0', '1', 'M']]
+    
+    assert real_board == expected_real_board, f"expted {expected_real_board}, but got {real_board}"
