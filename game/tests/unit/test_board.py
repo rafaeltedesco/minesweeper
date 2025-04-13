22a0d6073b9add9658cd4c74d5f09a7433539e51
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2025-04-13 16:55:36 -0300

    test: should dispatch game over event and reveal mine cell

--- game/tests/unit/test_board.py
+++ game/tests/unit/test_board.py
@@ -1,5 +1,6 @@
 import pytest
 from game.src.core.board import Board
+from game.src.core.events import Events
 
 def should_create_visible_board_given_fixed_number_of_rows_and_cols():
     n_rows = n_cols = 3
@@ -38,3 +39,11 @@ def should_precompute_adjacent_mines_and_return_real_board_given_a_visible_board
     expected_real_board = [['0', '1', '1'], ['0', '2', 'M'], ['0', '2', 'M']]
     assert board.real_board == expected_real_board, f"expected {expected_real_board}, but got {board.real_board}"
     
+def should_reveal_cell_and_dispatch_game_over_event_given_location_selected_contains_mine():
+    rows = cols = 3
+    board = Board(rows=rows, cols=cols, mines_count=2, seed=42)
+    row, col = board.mines_positions.pop()
+
+    event = board.reveal_cell(row, col)
+    assert event == Events.GAME_OVER
+    assert board.visible_board[row][col] == Board.BoardConsts.MINE
