d6be2faaa866273626a5b747b7f39892cb2b523f
Rafael Tedesco <dev.rafaeltedesco@gmail.com>
2025-04-13 14:22:00 -0300

    refactor: create OOP board

--- game/src/core/board.py
+++ game/src/core/board.py
@@ -1,3 +1,68 @@
-def create_visible_board(rows: int, cols: int) -> list[list[str]]:
-    visible_board = [['.' for _ in range(cols)] for _ in range(rows)]
-    return visible_board
+import random
+from copy import deepcopy
+from typing import Optional
+
+
+class Board:
+
+    def __init__(self, rows: int, cols: int, mines_count: int = 10, seed: Optional[int] = None):
+        self._rows = rows
+        self._cols = cols
+        self._mines_count = mines_count
+        self._visible_board = self._create_visible_board()
+        self._mines_positions = self._randomly_generate_mines(seed=seed)
+        self._real_board = self._precompute_adjacent_mines()
+
+    @property
+    def mines_positions(self) -> set[tuple[int, int]]:
+        return self._mines_positions
+    
+    @property
+    def visible_board(self) -> list[list[str]]:
+        return self._visible_board[:]
+    
+    @property
+    def real_board(self) -> list[list[str]]:
+        return self._real_board
+
+    @property
+    def size(self) -> tuple[int, int]:
+        return (self._rows, self._cols)
+
+    def _create_visible_board(self) -> list[list[str]]:
+        visible_board = [['.' for _ in range(self._cols)] for _ in range(self._rows)]
+        return visible_board
+    
+    def _randomly_generate_mines(self, seed: Optional[int] = None) -> set[tuple[int, int]]:
+        if self._mines_count > (self._rows * self._cols) // 2:
+            raise ValueError('"mine_counts" cannot be greather than 50% of the board size')
+
+        if seed is not None:
+            random.seed(seed)
+
+        mines_positions = set()
+        while len(mines_positions) < self._mines_count:
+            row, col = random.randint(0, self._rows - 1), random.randint(0, self._cols -1)
+            if (row, col) not in mines_positions:
+                mines_positions.add((row, col))
+
+        return mines_positions
+
+    def _precompute_adjacent_mines(self) -> list[list[str]]:
+        """
+            Given a visible board, it will precompute all adjacent mines and returns the real board
+        """
+        real_board = deepcopy(self._visible_board)
+        directions = [(-1, -1), (-1, 0), (-1, 1), (1, 0), (1, -1), (1, 1), (0, -1), (0, 1)]
+        for row in range(self._rows):
+            for col in range(self._cols):
+                adjacent_mines = 0
+                if (row, col) in self._mines_positions:
+                    real_board[row][col] = "M"
+                    continue
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if 0 <= nr < self._rows and 0 <= nc < self._cols and (nr, nc) in self._mines_positions:
+                        adjacent_mines += 1
+                real_board[row][col] = str(adjacent_mines)
+        return real_board
--- game/tests/unit/test_board.py
+++ game/tests/unit/test_board.py
@@ -1,9 +1,10 @@
-from game.src.core.board import create_visible_board
+import pytest
+from game.src.core.board import Board
 
 def should_create_visible_board_given_fixed_number_of_rows_and_cols():
     n_rows = n_cols = 3
     
-    visible_board = create_visible_board(rows=n_rows, cols=n_cols)
+    board = Board(rows=n_rows, cols=n_cols, mines_count=3, seed=42)
     
     expected_n_rows = expected_n_cols = 3
     expected_visible_board = [
@@ -11,9 +12,29 @@ def should_create_visible_board_given_fixed_number_of_rows_and_cols():
         ['.', '.', '.'],
         ['.', '.', '.']
     ]
-
-    rows, cols = len(visible_board), len(visible_board[0])
     
+    rows, cols = board.size
     assert rows == expected_n_rows, f"expected {expected_n_rows}, but got {rows}"
     assert cols == expected_n_cols, f"expected {expected_n_cols}, but got {cols}"
-    assert visible_board == expected_visible_board, "visible board is not equal expected visible board"
+    assert board.visible_board == expected_visible_board, "visible board is not equal expected visible board"
+
+
+def should_randomly_generate_mines_positions_given_rows_cols_and_a_number_of_expected_mines():
+    expected_positions = {(3, 8), (4, 3), (0, 0), (1, 8), (1, 0), (1, 6), (3, 2), (6, 3), (1, 3), (0, 8)}
+    rows = cols = 9
+    seed = 42
+    board = Board(rows, cols, mines_count=10, seed=seed)
+    mines_positions = board._mines_positions
+    assert mines_positions == expected_positions
+
+def should_prevent_mines_count_to_be_greather_than_fifty_percent_of_the_board_size_given_mines_count_provided():
+    rows = cols = 10
+    Board(rows, cols, mines_count=49)
+    with pytest.raises(ValueError, match='"mine_counts" cannot be greather than 50% of the board size'):
+        Board(rows, cols, mines_count=51)
+
+def should_precompute_adjacent_mines_and_return_real_board_given_a_visible_board():
+    board = Board(rows=3, cols=3, mines_count=2, seed=44)
+    expected_real_board = [['0', '1', '1'], ['0', '2', 'M'], ['0', '2', 'M']]
+    assert board.real_board == expected_real_board, f"expected {expected_real_board}, but got {board.real_board}"
+    
